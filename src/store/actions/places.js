bf2a5f2c26ed09f850d68fb3af26a43cf4220f20
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2018-04-12 19:11:07 +0300

    Add firebase function. Store files in Firebase Storage.

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,18 +1,29 @@
 import { DELETE_PLACE } from './actionTypes';
 
-export const addPlace = (placeName, location) => () => {
-  const placeData = {
-    name: placeName,
-    location,
-  };
-  fetch('https://awesome-places-a0a92.firebaseio.com/places.json', {
+export const addPlace = (placeName, location, image) => () => {
+  fetch('https://us-central1-awesome-places-a0a92.cloudfunctions.net/storeImage', {
     method: 'POST',
-    body: JSON.stringify(placeData),
+    body: JSON.stringify({
+      image: image.base64,
+    }),
   })
     .catch((err) => console.log(`Error: ${err}`))
     .then((res) => res.json())
-    .then((parsedResponse) => {
-      console.log('parsedResponse: ', parsedResponse);
+    .then((parsedRes) => {
+      const placeData = {
+        name: placeName,
+        image: parsedRes.imageUrl,
+        location,
+      };
+      return fetch('https://awesome-places-a0a92.firebaseio.com/places.json', {
+        method: 'POST',
+        body: JSON.stringify(placeData),
+      });
+    })
+    .catch((err) => console.log(`Error: ${err}`))
+    .then((res) => res.json())
+    .then((parsedRes) => {
+      console.log('parsedRes: ', parsedRes);
     });
 };
 
