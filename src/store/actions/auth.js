ce14b9ed7a94764c1dd2d71c5c03df5bd0111f28
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2018-04-17 17:27:06 +0300

    Sign User Up.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,28 @@
-import { TRY_AUTH } from './actionTypes';
+export const authSignup = (authData) => () => {
+  fetch(
+    'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCkl7rV-ytzH025wy8vPiYZuJzmhhMcrjU',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+      alert('Authentication failed, please try again!');
+    })
+    .then((res) => res.json())
+    .then((parseRes) => {
+      console.log('parseRes: ', parseRes);
+    });
+};
 
-export const tryAuth = (authData) => ({
-  type: TRY_AUTH,
-  authData,
-});
-
-export const unused = () => {};
+export const tryAuth = (authData) => (dispatch) => {
+  dispatch(authSignup(authData));
+};
